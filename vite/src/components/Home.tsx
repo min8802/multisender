551372e3555efc3b7602338a1a9f5f7d7b5cead3
min8802 <ww330@naver.com>
2024-10-04 15:11:33 +0900

    header done

---
+++ vite/src/components/Home.tsx
@@ -0,0 +1,12 @@
+import { Flex } from "@chakra-ui/react";
+import { FC } from "react";
+
+const Home : FC = () => {
+    return (
+        <Flex>
+            home
+        </Flex>
+    )
+}
+
+export default Home;
