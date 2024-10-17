cb60ea16b6a3f19ffc6bcc7d66efb9d72df86e7f
min8802 <ww330@naver.com>
2024-10-17 14:49:38 +0900

    change network

---
+++ vite/src/components/Header.tsx
@@ -0,0 +1,109 @@
+import { Button, Flex, Text } from "@chakra-ui/react";
+import { Dispatch, FC, SetStateAction, useEffect } from "react";
+import { JsonRpcSigner, ethers } from "ethers";
+import { useNavigate } from "react-router-dom";
+import HeaderMenu from "./HeaderMenu";
+import Navbar from "./Navbar";
+
+
+
+interface HeaderProps {
+  signer: JsonRpcSigner | null;
+  setSigner: Dispatch<SetStateAction<JsonRpcSigner | null>>;
+}
+
+const Header: FC<HeaderProps> = ({ signer, setSigner }) => {
+
+
+  const navigator = useNavigate();
+
+  const getSigner = async () => {
+    try {
+      if (!window.ethereum) return;
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      setSigner(await provider.getSigner());
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+  const onClickMetamaskLogin = async () => {
+    try {
+        await window.ethereum.request({method : 'wallet_requestPermissions', params:[{eth_accounts: {}}]});//지갑 허가 다시 받기
+        await getSigner();
+        //getSigner 함수를 wallet_requestPermissions 아래로 옮김, 변경된 주소가 렌딩이 됨
+        // localStorage.setItem("isLogin", "true");      
+    } catch (error) {
+      console.error("onClickMetamaskLogin Error : ",error);
+    }
+  };
+
+
+  useEffect(() => {
+    if(!signer)return;
+    console.log(signer.address);
+  },[signer])
+
+  return (
+    <Flex
+      position="fixed"
+      top={0}
+      left={0}
+      w="full"
+      h="70px"
+      justifyContent="space-between"
+      alignItems="center"
+      fontWeight="bold"
+      borderBottomColor="white"
+      borderBottomWidth="2px"
+      px={4}
+      zIndex={2}
+      bgColor="gray.800"
+    >
+      <Flex gap={24} position="relative">
+        <Button
+          variant="ghost"
+          w={220}
+          fontSize="20px"
+          textAlign="center"
+          alignSelf="center"
+          color="white"
+          fontWeight="bold"
+          _hover={{ transform: "scale(1.05)" }}
+          _active={{ transform: "scale(0.95)" }}
+          onClick={() => navigator("/")}
+        >
+          Unideca
+        </Button>
+        <Navbar/>
+      </Flex>
+      <Flex>
+        {signer ? (
+          <Flex alignItems="center" gap={4}>
+            <HeaderMenu
+              key={signer.address}//signer값이 변경되면 컴포넌트 리마운트
+              text={`${signer.address.slice(0,5)}...${signer.address.slice(-4)}`}
+              menuItem={["마이페이지", "로그아웃"]}
+              setSigner={setSigner}
+            />
+          </Flex>
+        ) : (
+          <Button
+            p={0}
+            bgColor="teal"
+            w="170px"
+            _hover={{ bg: "teal.400" }}
+            onClick={onClickMetamaskLogin}
+          >
+            <Text color="white" fontWeight="bold">
+              지갑 연결
+            </Text>
+          </Button>
+        )}
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Header;
---
+++ vite/src/components/NetworkSelector.tsx
@@ -0,0 +1,38 @@
+import { Select } from "@chakra-ui/react";
+import { FC, useEffect } from "react";
+
+interface NetworkSelectorProps {
+    network : string;
+    networkHandler : (e: React.ChangeEvent<HTMLSelectElement>) => Promise<void>;
+    isValidNetwork : boolean | null;
+}
+
+const NetworkSelector : FC<NetworkSelectorProps> = ({network, networkHandler, isValidNetwork}) => {
+    
+    useEffect(() => {
+        console.log(isValidNetwork)
+    },[isValidNetwork]);
+
+    return (
+        <Select
+              mt="4"
+              variant="ghost"
+              borderColor="brand.500"
+              borderWidth="2px"
+              cursor="pointer"
+              value={isValidNetwork ? network : "none"}
+              onChange={networkHandler}
+            >
+              <option value="0xa4b1">Arbitrum</option>
+              <option value="0xa86a">Avalanche</option>
+              <option value="0x2105">Base</option>
+              <option value="0x38">BSC</option>
+              <option value="0xfa">Fantom</option>
+              <option value="0xa">Optimism</option>
+              <option value="0x89">Polygon</option>
+              <option value="none">지원하지 않는 네트워크</option>
+            </Select>
+    )
+}
+
+export default NetworkSelector;
